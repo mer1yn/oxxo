a2982162858ba1ccb287e60debe160063a9a7cdf
Marco Vinicio <mvglizarraga@gmail.com>
2018-06-30 13:46:45 -0500

    uwu

--- script.js
+++ script.js
@@ -1,70 +1,33 @@
-var titleElement = document.getElementById('title');
-var descriptionElement = document.getElementById('description');
-var priorityElement = document.getElementById('form').priority;
-var buttonElement = document.getElementById('button');
-var items = document.getElementById('items');
+var productElement = document.getElementById('product');
 
-var arreglo = [];
+productElement.addEventListener('keyup', function(e) {
+  console.log(e);
+})
 
-function Tarea(title, description, priority) {
-  this.title = title;
-  this.description = description;
-  this.priority = priority;
-  this.isFinished = false;
-}
-
-buttonElement.addEventListener('click', function(){
-  var title = titleElement.value;
-  var description = descriptionElement.value;
-  var priority = priorityElement.value;
-
-  var x = new Tarea(title, description, priority);
-  arreglo.push(x);
-  renderElements();
-});
-
-/*
-<article class="item">
-  <h2>Titulo</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, at? Totam, quidem similique mollitia est odit, numquam repellat rerum sint.</p>
-  <span class="check">
-    <input type="checkbox">
-    <label for=""></label>
-  </span>
-  <button>Eliminar</button>
-</article>
-*/
-
-function renderElements() {
-  items.innerHTML = "";
-  for (var i = 0; i < arreglo.length; i++) {
-    items.innerHTML += returnItem(arreglo[i], i);
+var items = [
+  {
+    id: 1,
+    name: "rufles",
+    price: 45.00
+  },
+  {
+    id: 2,
+    name: "agua embotellada",
+    price: 10.00
+  },
+  {
+    id: 3,
+    name: "cafe",
+    price: 15.00
+  },
+  {
+    id: 4,
+    name: "pepsi",
+    price: 20.00
+  },
+  {
+    id: 5,
+    name: "kitkat",
+    price: 60.00
   }
-}
-
-function returnItem(tarea, index) {
-  return '<article class="item">'+
-  '<h2>'+tarea.title+'</h2>'+
-  '<p>'+tarea.description+'</p>'+
-  '<span class="check">'+
-  '<input type="checkbox">'+
-  '<label for=""></label>' +
-  '</span>'+
-  '<button onclick="eliminar('+index+')">Eliminar</button>'+
-  '</article>'
-}
-
-function eliminar(index) {
-  console.log('estas eliminado ' + arreglo[index].title);
-  arreglo.splice(index,1);
-  renderElements();
-}
-
-
-
-
-
-
-
-
-//
+];
